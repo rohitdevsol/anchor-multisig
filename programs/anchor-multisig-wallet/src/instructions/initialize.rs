3f422b9c3195a6668163027a6ffca8c9f61a5234
Rohit <rohitnarnolia88@gmail.com>
2026-04-14 20:40:05 +0530

    add initialize instruction

--- programs/anchor-multisig-wallet/src/instructions/initialize.rs
+++ programs/anchor-multisig-wallet/src/instructions/initialize.rs
@@ -1,9 +1,51 @@
 use anchor_lang::prelude::*;
 
-#[derive(Accounts)]
-pub struct Initialize {}
+use crate::{ MultisigConfig, error::ErrorCode };
 
-pub fn handler(ctx: Context<Initialize>) -> Result<()> {
-    msg!("Greetings from: {:?}", ctx.program_id);
-    Ok(())
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub creator: Signer<'info>,
+
+    #[account(
+        init,
+        payer = creator,
+        space = 8 + MultisigConfig::INIT_SPACE,
+        seeds = [b"multisig".as_ref(), creator.key().as_ref()],
+        bump
+    )]
+    pub config: Account<'info, MultisigConfig>,
+
+    #[account(
+        init,
+        space = 0,
+        payer = creator,
+        seeds = [b"vault".as_ref(), config.key().as_ref()],
+        bump
+    )]
+    pub vault: UncheckedAccount<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Initialize<'info> {
+    pub fn create_wallet(
+        &mut self,
+        threshold: u8,
+        others: Vec<Pubkey>,
+        bumps: InitializeBumps
+    ) -> Result<()> {
+        // just a custom setting for the threshold
+        require!(threshold > 0 && threshold <= 10, ErrorCode::InvalidThresholdValue);
+
+        self.config.set_inner(MultisigConfig {
+            bump: bumps.config,
+            creator: self.creator.key(),
+            label: "".to_string(),
+            threshold,
+            signers: others,
+        });
+
+        Ok(())
+    }
 }
